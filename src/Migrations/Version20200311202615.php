4ab00a21fb08b29ef966704a9e19e9504fe20b16
rampinflorian <contact@florianrampin.fr>
2020-03-11 21:27:27 +0100

    water quality
    Signed-off-by: rampinflorian <contact@florianrampin.fr>

---
+++ src/Migrations/Version20200311202615.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200311202615 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE water_quality (id INT AUTO_INCREMENT NOT NULL, ph DOUBLE PRECISION NOT NULL, kh DOUBLE PRECISION NOT NULL, co2 DOUBLE PRECISION NOT NULL, no2 DOUBLE PRECISION NOT NULL, no3 DOUBLE PRECISION NOT NULL, fe DOUBLE PRECISION NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE rdv_planning CHANGE client_id client_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE water_quality');
+        $this->addSql('ALTER TABLE rdv_planning CHANGE client_id client_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
