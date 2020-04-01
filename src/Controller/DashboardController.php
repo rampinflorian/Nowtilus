35c9e7b2517c68a6540730ef67bf9ee2d1e17336
rampinflorian <contact@florianrampin.fr>
2020-04-01 23:13:16 +0200

    full API WaterQualityController.php
    Signed-off-by: rampinflorian <contact@florianrampin.fr>

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use FeedIo\FeedIo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/", name="dashboard")
+     * @param FeedIo $feedIo
+     * @return Response
+     * @throws \Exception
      */
-    public function index()
+    public function index(FeedIo $feedIo)
     {
+
+        $result = $feedIo->read('https://www.bfmtv.com/rss/info/flux-rss/flux-toutes-les-actualites/', null, new \DateTime('- 1 days'))->getFeed();
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'result' => $result
         ]);
     }
 }
